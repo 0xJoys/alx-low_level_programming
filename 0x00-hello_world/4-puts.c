f5dcd967bd6494190541f593d10b9662a7dc0e4d
Joys <ajisefinnijoy@gmail.com>
2022-09-11 09:59:44 -0700

    Hello put

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+
+
+/**
+ * main - Entry point
+ * 
+ * Return: Alway 0 (Success)
+ *
+ */
+
+
+
+int main (void)
+
+{
+
+	puts("\"Progmamming is like building a building a multilingual puszzle");
+
+		return (0);
+
+}
