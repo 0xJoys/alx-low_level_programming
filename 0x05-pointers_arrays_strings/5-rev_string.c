10141ce1397c690cdec59a90f8696318cedf892e
Joys <ajisefinnijoy@gmail.com>
2022-09-20 05:50:14 -0700

    reverse string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -11,10 +11,10 @@ char temp;
 for (len = 0; s[len] != '\0'; len++)
 i = 0;
 half = len / 2;
-while (half--2)
+while (half--)
 {
 temp = s[len - i 1];
-s[len - i - 1] = s [i];
+s[len - i - 1] = s[i];
 s[i] = temp;
 i++;
 }
