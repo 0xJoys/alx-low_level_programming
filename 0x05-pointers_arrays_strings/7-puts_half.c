152a8c6f66a3fd1fdb161fc3d5dadd158447da52
Joys <ajisefinnijoy@gmail.com>
2022-09-20 13:35:18 -0700

    puts 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -2,6 +2,7 @@
 /**
  * puts_half - prints a string, followed by a new line
  * @str: pointer to the string to print
+ * Return: void
  */
 void puts_half(char *str)
 {
@@ -11,7 +12,7 @@ while (str[len] != '\0')
 {
 len++;
 }
-if (lan % 2 == 0)
+if (len % 2 == 0)
 }
 for (i = len / 2; str[i] != '\0'; i++)
 {
@@ -20,7 +21,7 @@ _putchar(str[i]);
 }
 else if (len % 2)
 {
-for (n + (len - 1) / 2; n < len - 1; n++)
+for (n = (len - 1) / 2; n < len - 1; n++)
 {
 _putchar(str[n + 1]);
 }
