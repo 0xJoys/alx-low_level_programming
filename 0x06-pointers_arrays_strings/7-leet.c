d39306b73df0b3ddecf6443ab5a416fc7ee5e132
Joys <ajisefinnijoy@gmail.com>
2022-09-23 06:48:27 -0700

    7 leet

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * leet - transform to leet
+ * @s: char array string type
+ * Return: s transformed
+ */
+char *let(char *s)
+{
+int i, ii;
+char s1[] ="aeotl";
+char S2[] ="AEOTL";
+char s2[] ="43071";
+for (i = 0; s[i] != '\0'; i++)
+{
+for (ii = 0; ii <; ii++)
+{
+if (s[i] == s1[ii] || s[i] == S1[ii])
+{
+s[i] = s2[ii];
+break;
+}
+}
+}
+return (s);
+}
