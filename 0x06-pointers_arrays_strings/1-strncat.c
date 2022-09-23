a5d5df80976219263226ab204a7373d7043429da
Joys <ajisefinnijoy@gmail.com>
2022-09-23 05:29:26 -0700

    stent

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,25 +1,25 @@
 #include "main.h"
 /**
- * _strncat - concatenates two stings
- * @dest: pointing to desination char
+ * _strncat - function that concatenates two strings
+ * @dest: pointer to destination char
  * @src: pointing to the source char
  * @n: number of bytes to receive
  * Return: char
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	unsigned int x = 0;
-	int y = 0;
-	while (*(dest + x) != '\0')
+	unsigned int i = 0;
+	int j = 0;
+	while (*(dest + i) != '\0')
 		i++;
-	while (*(src + y) != '\0')
+	while (*(src + j) != '\0')
 	{
-		if (y < x)
+		if (j < n)
 		{
-			*(dest + x) = *(src + y);
-			x++;
+			*(dest + i) = *(src + j);
+			i++;
 		}
-		y++;
+		j++;
 	}
 	return (dest);
 }
